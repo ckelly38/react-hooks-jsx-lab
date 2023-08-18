dc3da75b0ff8e42eb3f802283153d2085449869d
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-18 04:14:38 -0600

    built components and added CSS style to it noting that I need a separate object with the attributes I want

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const styleobj = {
+    color: "firebrick"
+  };
+
+  return (
+  
+  <div id="home">
+    <h1 style={styleobj}>{name} is a Web Developer from {city}</h1>
+    </div>
+    
+  );
 }
 
 export default Home;
